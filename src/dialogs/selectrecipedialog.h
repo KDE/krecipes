0cf62a28790fd97e959beb227c08c49676b6e47e
Unai Garro <ugarro@gmail.com>
2003-05-16 01:38:06 +0000

    Reimport everything trying to fix the mess I did before. Hopefully this time will be ok. Happy downloading :-)
    svn path=/trunk/kdeextragear-3/krecipes/; revision=226097

---
+++ src/dialogs/selectrecipedialog.h
@@ -0,0 +1,27 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by Unai Garro                                      *
+ *   uga@ee.ed.ac.uk                                                       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+#ifndef SELECTRECIPEDIALOG_H
+#define SELECTRECIPEDIALOG_H
+
+#include <qvbox.h>
+
+/**
+@author Unai Garro
+*/
+class SelectRecipeDialog : public QVBox
+{
+public:
+    SelectRecipeDialog();
+
+    ~SelectRecipeDialog();
+
+};
+
+#endif
