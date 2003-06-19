f1708141a9b9438e028d353eea13c224facdd49c
Unai Garro <ugarro@gmail.com>
2003-06-19 11:35:16 +0000

    Prototype for a new Setup Wizard
    svn path=/trunk/kdeextragear-3/krecipes/; revision=232314

---
+++ src/setupwizard.h
@@ -0,0 +1,26 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by Unai Garro                                      *
+ *   uga@ee.ed.ac.uk                                                       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+#ifndef SETUPWIZARD_H
+#define SETUPWIZARD_H
+
+#include <kwizard.h>
+
+/**
+@author Unai Garro
+*/
+class SetupWizard:KWizard{
+public:
+
+     SetupWizard(QWidget *parent);
+    ~SetupWizard();
+
+};
+
+#endif
