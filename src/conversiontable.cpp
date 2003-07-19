8285c4302581a1e30430ae0c385127d18f32bd60
Unai Garro <ugarro@gmail.com>
2003-07-19 23:27:38 +0000

    Oh, I forgot, in the previous commit I also put back the option to set values only when "Ok" is pressed, otherwise the value changes constantly whenever we change the amount...
    svn path=/trunk/kdeextragear-3/krecipes/; revision=238069

--- src/conversiontable.cpp
+++ src/conversiontable.cpp
@@ -7,6 +7,7 @@
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  ***************************************************************************/
+
 #include "conversiontable.h"
 #include <iostream>
 
