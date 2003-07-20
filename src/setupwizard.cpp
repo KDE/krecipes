eabd21768e6795f125f2b87391fe58504b5bdafc
Unai Garro <ugarro@gmail.com>
2003-07-20 21:46:00 +0000

    Set default values for database in wizard, and improve layout.
    svn path=/trunk/kdeextragear-3/krecipes/; revision=238232

--- src/setupwizard.cpp
+++ src/setupwizard.cpp
@@ -1,12 +1,15 @@
 /***************************************************************************
  *   Copyright (C) 2003 by Unai Garro                                      *
- *   ugarro@users.sourceforge.net                                                       *
+ *   ugarro@users.sourceforge.net                                          *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  ***************************************************************************/
+
+#include <unistd.h>
+#include <pwd.h>
 
 #include <iostream>
 #include <qlayout.h>
@@ -39,9 +42,9 @@ SetupWizard::~SetupWizard()
 WelcomePage::WelcomePage(QWidget *parent):QWidget(parent)
 {
 QGridLayout *layout=new QGridLayout(this,1,1,0,0);
-QSpacerItem *spacer_top=new QSpacerItem(5,5,QSizePolicy::Minimum, QSizePolicy::Fixed);
+QSpacerItem *spacer_top=new QSpacerItem(10,10,QSizePolicy::Minimum, QSizePolicy::Fixed);
 layout->addItem(spacer_top,0,1);
-QSpacerItem *spacer_left=new QSpacerItem(5,5,QSizePolicy::Fixed, QSizePolicy::Minimum);
+QSpacerItem *spacer_left=new QSpacerItem(10,10,QSizePolicy::Fixed, QSizePolicy::Minimum);
 layout->addItem(spacer_left,1,0);
 QPixmap logoPixmap (locate("data", "krecipes/pics/wizard.png"));
 logo=new QLabel(this);
@@ -49,13 +52,16 @@ logo->setPixmap(logoPixmap);
 logo->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
 layout->addWidget(logo,1,1);
 
+QSpacerItem *spacer_from_image=new QSpacerItem(10,10,QSizePolicy::Fixed, QSizePolicy::Minimum);
+layout->addItem(spacer_from_image,1,2);
+
 welcomeText=new QLabel(this);
 welcomeText->setText("Thanks very much for choosing Krecipes.\nIt looks like this is the first time you are using it. This wizard will help you with the initial setup so that you can start using it quickly.\n\nWelcome and enjoy cooking! ;-) ");
 welcomeText->setMinimumWidth(200);
 welcomeText->setMaximumWidth(10000);
-welcomeText->setSizePolicy(QSizePolicy::Minimum,QSizePolicy::Minimum);
-welcomeText->setAlignment( int( QLabel::WordBreak | QLabel::AlignVCenter ) );
-layout->addWidget(welcomeText,1,2);
+welcomeText->setSizePolicy(QSizePolicy::MinimumExpanding,QSizePolicy::Minimum);
+welcomeText->setAlignment( int( QLabel::WordBreak | QLabel::AlignTop |QLabel::AlignJustify  ) );
+layout->addWidget(welcomeText,1,3);
 
 }
 
@@ -69,63 +75,74 @@ QSpacerItem *spacer_left=new QSpacerItem(10,10,QSizePolicy::Fixed, QSizePolicy::
 layout->addItem(spacer_left,1,0);
 
 
+
 QPixmap networkSetupPixmap (locate("data", "krecipes/pics/network.png"));
 logo=new QLabel(this);
 logo->setPixmap(networkSetupPixmap);
 logo->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
 layout->addMultiCellWidget(logo,1,8,1,1);
 
+QSpacerItem *spacer_from_image=new QSpacerItem(10,10,QSizePolicy::Fixed, QSizePolicy::Minimum);
+layout->addItem(spacer_from_image,1,2);
+
 QLabel* serverText=new QLabel("Server:", this);
 serverText->setFixedSize(QSize(100,20));
 serverText->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
-layout->addWidget(serverText,1,2);
+layout->addWidget(serverText,1,3);
 
 serverEdit=new KLineEdit(this);
 serverEdit->setFixedSize(QSize(120,20));
 serverEdit->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
-layout->addWidget(serverEdit,1,3);
+serverEdit->setText("localhost");
+layout->addWidget(serverEdit,1,4);
 
 QSpacerItem* spacerRow1 = new QSpacerItem( 10,10, QSizePolicy::Minimum, QSizePolicy::Fixed );
-layout->addItem( spacerRow1,2,2 );
+layout->addItem( spacerRow1,2,3 );
 
 QLabel* usernameText=new QLabel("Username:", this);
 usernameText->setFixedSize(QSize(100,20));
 usernameText->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
-layout->addWidget(usernameText,3,2);
+layout->addWidget(usernameText,3,3);
 
 usernameEdit=new KLineEdit(this);
 usernameEdit->setFixedSize(QSize(120,20));
 usernameEdit->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
-layout->addWidget(usernameEdit,3,3);
+	// get username
+	uid_t userID; QString username;struct passwd *user;
+	userID=getuid();user=getpwuid (userID); username=user->pw_name;
+
+usernameEdit->setText(username);
+layout->addWidget(usernameEdit,3,4);
 
 QSpacerItem* spacerRow2 = new QSpacerItem( 10,10, QSizePolicy::Minimum, QSizePolicy::Fixed );
-layout->addItem( spacerRow2,4,2 );
+layout->addItem( spacerRow2,4,3 );
 
 QLabel* passwordText=new QLabel("Password:", this);
 passwordText->setFixedSize(QSize(100,20));
 passwordText->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
-layout->addWidget(passwordText,5,2);
+layout->addWidget(passwordText,5,3);
 
 passwordEdit=new KLineEdit(this);
 passwordEdit->setFixedSize(QSize(120,20));
 passwordEdit->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
-layout->addWidget(passwordEdit,5,3);
+layout->addWidget(passwordEdit,5,4);
 
 QSpacerItem* spacerRow3 = new QSpacerItem( 10,10, QSizePolicy::Minimum, QSizePolicy::Fixed );
-layout->addItem( spacerRow3, 6,2 );
+layout->addItem( spacerRow3, 6,3 );
 
 QLabel* dbNameText=new QLabel("Database Name:", this);
 dbNameText->setFixedSize(QSize(100,20));
 dbNameText->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
-layout->addWidget(dbNameText,7,2);
+layout->addWidget(dbNameText,7,3);
 
 dbNameEdit=new KLineEdit(this);
 dbNameEdit->setFixedSize(QSize(120,20));
 dbNameEdit->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
-layout->addWidget(dbNameEdit,7,3);
+dbNameEdit->setText("Krecipes");
+layout->addWidget(dbNameEdit,7,4);
 
 QSpacerItem* spacerRow4 = new QSpacerItem( 10,10, QSizePolicy::Minimum, QSizePolicy::MinimumExpanding);
-layout->addItem( spacerRow4, 8,2 );
+layout->addItem( spacerRow4, 8,3 );
 QSpacerItem* spacerRight = new QSpacerItem( 10,10, QSizePolicy::MinimumExpanding, QSizePolicy::Fixed );
 layout->addItem( spacerRight, 1,5 );
 }
@@ -164,14 +181,17 @@ logo->setPixmap(logoPixmap);
 logo->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
 layout->addWidget(logo,1,1);
 
+QSpacerItem *spacer_from_image=new QSpacerItem(10,10,QSizePolicy::Fixed, QSizePolicy::Minimum);
+layout->addItem(spacer_left,1,2);
+
 saveText=new QLabel(this);
 saveText->setText("Congratulations! The necessary configuration was already setup. Press 'Finish' to continue, and enjoy cooking!");
 saveText->setMinimumWidth(200);
 saveText->setMaximumWidth(10000);
 saveText->setSizePolicy(QSizePolicy::Minimum,QSizePolicy::Minimum);
 
-saveText->setAlignment( int( QLabel::WordBreak | QLabel::AlignVCenter ) );
-layout->addWidget(saveText,1,2);
+saveText->setAlignment( int( QLabel::WordBreak | QLabel::AlignVCenter | QLabel::AlignJustify) );
+layout->addWidget(saveText,1,3);
 
 
 }
