64a1e75d67050f19c90267ea877b3e0971693843
Unai Garro <ugarro@gmail.com>
2003-08-02 16:46:40 +0000

    New class SelectCategoriesDialog
    svn path=/trunk/kdeextragear-3/krecipes/; revision=240565

---
+++ src/dialogs/selectcategoriesdialog.h
@@ -0,0 +1,30 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by Unai Garro                                      *
+ *   ugarro@users.sourceforge.net                                          *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+#ifndef SELECTCATEGORIESDIALOG_H
+#define SELECTCATEGORIESDIALOG_H
+
+#include <qwidget.h>
+#include "recipedb.h"
+
+/**
+@author Unai Garro
+*/
+class SelectCategoriesDialog:public QWidget{
+
+Q_OBJECT
+
+public:
+
+    SelectCategoriesDialog(QWidget *parent, RecipeDB *db);
+    ~SelectCategoriesDialog();
+
+};
+
+#endif
