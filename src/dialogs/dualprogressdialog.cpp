fc76045fa0bcbe65d5e768c68123bc1e82fdc787
Jason Kivlighn <jkivlighn@gmail.com>
2003-09-04 22:50:51 +0000

    DualProgressDialog class
    svn path=/trunk/kdeextragear-3/krecipes/; revision=248418

---
+++ src/dialogs/dualprogressdialog.cpp
@@ -0,0 +1,65 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by krecipes.sourceforge.net authors                *
+ *                                                                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#include "dualprogressdialog.h"
+
+#include <qlayout.h>
+#include <qlabel.h>
+
+DualProgressDialog::DualProgressDialog( QWidget* parent, const char* name, const QString& caption, const QString& text,     const QString &sub_text, bool modal ) : KProgressDialog( parent, name, caption, text, modal ),
+  m_current_step(0),
+  m_total_steps(1)
+{
+	QFrame* mainWidget = plainPage();
+	QLayout* layout = mainWidget->layout();
+
+	m_sub_label = new QLabel( sub_text, mainWidget);
+	layout->add(m_sub_label);
+
+	m_sub_progress_bar = new KProgress(mainWidget);
+	layout->add(m_sub_progress_bar);
+
+	connect(m_sub_progress_bar, SIGNAL(percentageChanged(int)),
+	  this, SLOT(slotUpdateTotalProgress(int)));
+
+	progressBar()->setTotalSteps( 100 );
+
+	slotAutoShow();
+}
+
+void DualProgressDialog::setSubLabel( const QString &text )
+{
+	m_sub_label->setText( text );
+}
+
+QString DualProgressDialog::subLabelText()
+{
+	return m_sub_label->text();
+}
+
+void DualProgressDialog::slotUpdateTotalProgress( int percentage )
+{
+	if ( percentage < 100 )
+	{
+		int total_percent = (double(m_current_step) + double(percentage)/100.0)/double(m_total_steps)*100.0;
+
+		progressBar()->setProgress( total_percent );
+	}
+	else
+	{
+		m_current_step++;
+		m_sub_progress_bar->setProgress(0);
+	}
+}
+
+void DualProgressDialog::setTotalSteps( int steps )
+{
+	m_total_steps = steps;
+}
---
+++ src/dialogs/dualprogressdialog.h
@@ -0,0 +1,51 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by krecipes.sourceforge.net authors                *
+ *                                                                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#ifndef DUALPROGRESSDIALOG_H
+#define DUALPROGRESSDIALOG_H
+
+#include <kprogress.h>
+
+#include <qobject.h>
+
+class QLabel;
+
+/** @brief A dialog that presents the user with a progress bar and a sub progress bar
+  *
+  * Set the total number of steps in the total progress bar, and then incrementing the progress
+  * in the sub progress bar will be increment the total progress bar accordingly.
+  * @author Jason Kivlighn
+  */
+class DualProgressDialog : public KProgressDialog
+{
+Q_OBJECT;
+
+public:
+	DualProgressDialog( QWidget* parent = 0, const char* name = 0, const QString& caption = QString::null, const QString& text = QString::null, const QString &sub_text = QString::null, bool modal = false );
+
+	KProgress *subProgressBar(){ return m_sub_progress_bar; };
+	void setSubLabel( const QString & );
+	void setTotalSteps( int steps );
+	QString subLabelText();
+
+private:
+	KProgress *m_sub_progress_bar;
+	QLabel *m_sub_label;
+	int m_current_step;
+	int m_total_steps;
+
+	//make this private
+	KProgress * progressBar(){ return KProgressDialog::progressBar(); }
+
+private slots:
+	void slotUpdateTotalProgress( int percentage );
+};
+
+#endif //DUALPROGRESSDIALOG_H
