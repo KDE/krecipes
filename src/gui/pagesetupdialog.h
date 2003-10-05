d8046a61127f6eaff52b6a7819dd186d8b09db77
Jason Kivlighn <jkivlighn@gmail.com>
2003-10-05 22:35:14 +0000

    Separated SetupDisplay to its own file and widget
    svn path=/trunk/kdeextragear-3/krecipes/; revision=256629

--- src/gui/pagesetupdialog.h
+++ src/gui/pagesetupdialog.h
@@ -13,60 +13,28 @@
 
 #include <qdialog.h>
 #include <qmap.h>
-#include <qvaluelist.h>
 
 #include "../recipe.h"
 
-class QPopupMenu;
-class QAction;
-class QLabel;
-
-class DragArea;
+class SetupDisplay;
 
 /**
   * @author Jason Kivlighn
   */
 class PageSetupDialog : public QDialog
 {
-Q_OBJECT;
+Q_OBJECT
 
 public:
 	PageSetupDialog( QWidget *parent, const Recipe &sample );
-	~PageSetupDialog();
+	~PageSetupDialog(){}
 
 protected:
 	virtual void accept();
 	void save();
 
-protected slots:
-	void widgetClicked( QMouseEvent *, QWidget * );
-
-	//slots to set properties of item boxes
-	void setBackgroundColor();
-	void setTextColor();
-	void setFont();
-	void setShown(int id);
-	void setAlignment( QAction * );
-
 private:
-	void loadSetup();
-	void createWidgets( const Recipe &sample );
-
-	DragArea *dragArea;
-
-	QLabel *title_box;
-	QLabel *instr_box;
-	QLabel *photo_box;
-	QLabel *servings_box;
-	QLabel *categories_box;
-	QLabel *authors_box;
-	QLabel *id_box;
-	QLabel *ingredients_box;
-
-	enum Properties { None = 0, BackgroundColor = 1, TextColor = 2, Font = 4, Visibility = 8, Geometry = 16, Alignment = 32 };
-	QMap< QWidget*, unsigned int > *box_properties;
-
-	QPopupMenu *popup;
+	SetupDisplay *setup_display;
 };
 
 #endif //PAGESETUPDIALOG_H
