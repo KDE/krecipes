c1fadbab2cacb31bcac13608270fe192e3e30405
Unai Garro <ugarro@gmail.com>
2003-10-06 19:33:36 +0000

    bye-bye, cute penguin ;(,,,
    svn path=/trunk/kdeextragear-3/krecipes/; revision=256828

--- src/setupwizard.cpp
+++ src/setupwizard.cpp
@@ -431,7 +431,7 @@ initializeText->setAlignment( int( QLabel::WordBreak | QLabel::AlignTop  ) );
 layout->addWidget(initializeText,1,3);
 
 // Logo
-QPixmap dataInitializePixmap (locate("data", "krecipes/pics/pengwithfork.png"));
+QPixmap dataInitializePixmap (locate("data", "krecipes/pics/recipes.png"));
 logo=new QLabel(this);
 logo->setPixmap(dataInitializePixmap);
 logo->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
