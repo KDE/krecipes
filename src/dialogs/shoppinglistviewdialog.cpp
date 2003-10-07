3a8fc5a4ed7c5f49743122af4801e73485c20a35
Unai Garro <ugarro@gmail.com>
2003-10-07 00:09:01 +0000

    CVS_SILENT Readd licenses
    svn path=/trunk/kdeextragear-3/krecipes/; revision=256884

--- src/dialogs/shoppinglistviewdialog.cpp
+++ src/dialogs/shoppinglistviewdialog.cpp
@@ -1,12 +1,15 @@
 /***************************************************************************
- *   Copyright (C) 2003 by Unai Garro                                      *
- *   ugarro@users.sourceforge.net                                          *
+ *   Copyright (C) 2003 by                                                 *
+ *   Unai Garro (ugarro@users.sourceforge.net)                             *
+ *   Cyril Bosselut (bosselut@b1project.com)                               *
+ *   Jason Kivlighn (mizunoami44@users.sourceforge.net)                    *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  ***************************************************************************/
+
 #include "shoppinglistviewdialog.h"
 #include "shoppingcalculator.h"
 #include "ingredientlist.h"
