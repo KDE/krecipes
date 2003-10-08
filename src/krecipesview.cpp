19a236509a2d102d41e44c4684db426f8a621b9c
Jason Kivlighn <jkivlighn@gmail.com>
2003-10-08 22:47:28 +0000

    Give confirmation before deleting a recipe
    Change header
    
    svn path=/trunk/kdeextragear-3/krecipes/; revision=257352

--- src/krecipesview.cpp
+++ src/krecipesview.cpp
@@ -1,6 +1,14 @@
-/*
- * Copyright (C) 2003 krecipes.sourceforge.net developers
- */
+/***************************************************************************
+ *   Copyright (C) 2003 by                                                 *
+ *   Unai Garro (ugarro@users.sourceforge.net)                             *
+ *   Cyril Bosselut (bosselut@b1project.com)                               *
+ *   Jason Kivlighn (mizunoami44@users.sourceforge.net)                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
 
 #include "krecipesview.h"
 
@@ -360,8 +368,16 @@ else if (action==1) // Edit
 }
 else if (action==2) //Remove
 {
-database->removeRecipe(recipeID);
-selectPanel->reload();
+	switch( KMessageBox::questionYesNo( this,
+	  QString(i18n("Are you sure you want to permanently remove the selected recipe?")),
+	  i18n("Confirm remove") ) )
+	{
+		case KMessageBox::Yes:
+			database->removeRecipe(recipeID);
+			selectPanel->reload();
+			break;
+		case KMessageBox::No: break;
+	}
 }
 }
 
