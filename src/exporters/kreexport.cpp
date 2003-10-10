541f11abfd2e8c2ed69dc0155dd0695b9e63a231
Unai Garro <ugarro@gmail.com>
2003-10-10 15:24:45 +0000

    CVS_SILENT Please someone tell me what I coded on this file... I feel embarrased to have my name right on top of a file in which I haven't touched a single char ;)
    svn path=/trunk/kdeextragear-3/krecipes/; revision=257783

--- src/exporters/kreexport.cpp
+++ src/exporters/kreexport.cpp
@@ -1,6 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2003 by krecipes.sourceforge.net authors                *
- *                                                                         *
+ *   Copyright (C) 2003 by Cyril Bosselut (bosselut@b1project.com)         *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
