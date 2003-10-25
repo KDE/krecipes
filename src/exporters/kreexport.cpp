7aa018426c23cc00871e6993f0a5903920fb056c
Jason Kivlighn <jkivlighn@gmail.com>
2003-10-25 03:51:43 +0000

    Use BaseExporter as superclass
    svn path=/trunk/kdeextragear-3/krecipes/; revision=261763

--- src/exporters/kreexport.cpp
+++ src/exporters/kreexport.cpp
@@ -8,31 +8,20 @@
  ***************************************************************************/
 #include "kreexport.h"
 
-#include <kdebug.h>
+#include <qfile.h>
 
-KreExporter::KreExporter( RecipeDB *db, const QString& filename = QString::null, const QString format = QString::null )
+#include <kdebug.h>
+#include <klocale.h>
+#include <kmdcodec.h>
+#include <ktar.h>
+#include <ktempfile.h>
+#include <kglobal.h>
+#include <kstandarddirs.h>
+
+KreExporter::KreExporter( RecipeDB *db, const QString& filename, const QString format ) :
+  BaseExporter( db, filename, format )
 {
-  database = db;
-  if(filename != QString::null){
-    if(filename.right(4) == ".kre" || filename.right(6) == ".kreml"){
-      file = new QFile(filename);
-    }
-    else{
-      QString form = format;
-      form = form.remove('*');
-      if(form != ".kre" && form != ".kreml"){
-        form = ".kre";
-      }
-      file = new QFile(filename+form);
-    }
-    QFileInfo fi(*file);
-    type = fi.extension();
-    name = (fi.fileName()).remove("."+type);
-    exportEnable = true;
-  }
-  else{
-    exportEnable = false;
-  }
+	possible_formats << ".kre" << ".kreml";
 }
 
 
@@ -40,116 +29,58 @@ KreExporter::~KreExporter()
 {
 }
 
-
-
-
-/*!
-    \fn KreExporter::export()
- * export recipe to XML and save it to a file
- */
-void KreExporter::exporter(const int recipeId)
+void KreExporter::saveToFile( const QPtrList<Recipe>& recipes )
 {
-  recipe = new Recipe();
-  recipe->recipeID = -1;
-  database->loadRecipe( recipe, recipeId );
-  if(exportEnable){
-    bool fileExists = file->exists();
-    int overwrite;
-    if(fileExists){
-      overwrite = KMessageBox::questionYesNo( 0,i18n("File ")+file->name()+i18n(" exists. Would you like to overwrite it?"),i18n("Saving recipe") );
-    }
-    if(!fileExists || overwrite == KMessageBox::Yes){
-      if(type == "kreml"){
-        if ( file->open( IO_WriteOnly ) ) {
-          QTextStream stream( file );
-          stream << "<krecipes version=\"0.4\" lang=\""+(KGlobal::locale())->country()+"\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"krecipes.xsd\">\n";
-          stream << createKRE();
-          stream << "</krecipes>";
-          file->close();
-        }
-      }
-      else{
+	if(format == "kreml")
+	{
+        	if ( file->open( IO_WriteOnly ) )
+		{
+			QTextStream stream( file );
+			stream << createKRE(recipes);
+			file->close();
+		}
+	}
+	else{
         // create a temporary .kre file
-        QString kreml = "<krecipes version=\"0.4\" lang=\""+(KGlobal::locale())->country()+"\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"krecipes.xsd\">\n";
-        kreml += createKRE();
-        kreml += "</krecipes>";
+        QString kreml = createKRE(recipes);
         int size = kreml.length();
         // compress and save file
         KTar* kre = new KTar(file->name(), "application/x-gzip");
         kre->open( IO_WriteOnly );
-        kre->writeFile(name+".kreml",getenv( "LOGNAME" ), "", size, kreml);
+        kre->writeFile(filename+".kreml",getenv( "LOGNAME" ), "", size, kreml);
         kre->close();
+	delete kre;
       }
-    }
-  }
-  else{
-    kdDebug()<<"no output file have been selected for export."<<endl;
-  }
 }
-
-void KreExporter::categoryExporter(QValueList<int>* l){
-  recipe = new Recipe();
-  recipe->recipeID = -1;
-  QString kreml;
-  QValueList<int>::iterator it;
-  if(exportEnable){
-    bool fileExists = file->exists();
-    int overwrite;
-    if(fileExists){
-      overwrite = KMessageBox::questionYesNo( 0,i18n("File ")+file->name()+i18n(" exists. Would you like to overwrite it?"),i18n("Saving recipe") );
-    }
-    if(!fileExists || overwrite == KMessageBox::Yes){
-      kreml = "<krecipes version=\"0.4\" lang=\""+(KGlobal::locale())->country()+"\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"krecipes.xsd\">\n";
-      for ( it = l->begin(); it != l->end(); ++it ){
-        database->loadRecipe( recipe, *it );
-        kreml += createKRE();
-      }
-      kreml += "</krecipes>\n";
-      if(type == "kreml"){
-        if ( file->open( IO_WriteOnly ) ) {
-          QTextStream stream( file );
-          stream << kreml;
-          file->close();
-        }
-      }
-      else{
-        int size = kreml.length();
-        // compress and save file
-        KTar* kre = new KTar(file->name(), "application/x-gzip");
-        kre->open( IO_WriteOnly );
-        kre->writeFile(name+".kreml",getenv( "LOGNAME" ), "", size, kreml);
-        kre->close();
-      }
-    }
-  }
-  else{
-    kdDebug()<<"no output file have been selected for export."<<endl;
-  }
-}
-
 
 /*!
     \fn KreManager::createKRE()
  * return a QString containing XML encoded recipe
  */
-QString KreExporter::createKRE(const int recipeId)
+QString KreExporter::createKRE( const QPtrList<Recipe>& recipes )
 {
     QString xml;
-    if(recipeId != -1){
-      recipe = new Recipe();
-      recipe->recipeID = -1;
-      database->loadRecipe( recipe, recipeId );
-      xml = "<krecipes version=\"0.4\" lang=\""+(KGlobal::locale())->country()+"\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"krecipes.xsd\">\n";
-    }
-    else{
-      xml = "";
-    }
+
+    xml = "<krecipes version=\"0.4\" lang=\""+(KGlobal::locale())->country()+"\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"krecipes.xsd\">\n";
+
+	QPtrListIterator<Recipe> recipes_it( recipes );
+	Recipe *recipe;
+	while ( (recipe = recipes_it.current()) != 0 )
+	{
+	++recipes_it;
+
     xml +="<krecipes-recipe>\n";
     xml += "<krecipes-description>\n";
     xml += "<title>"+recipe->title.utf8()+"</title>\n";
-    for (Element *el = (recipe->authorList).getFirst(); el; el= (recipe->authorList).getNext()){
-      xml += "<author>"+el->name.utf8()+"</author>\n";
-    }
+
+	QPtrListIterator<Element> author_it( recipe->authorList );
+	Element *author;
+	while ( (author = author_it.current()) != 0 )
+	{
+		++author_it;
+		xml += "<author>"+author->name.utf8()+"</author>\n";
+	}
+
     xml += "<pictures>\n";
     xml += "<pic format=\"JPEG\" id=\"1\"><![CDATA["; //fixed id until we implement multiple photos ability
     KTempFile* fn = new KTempFile (locateLocal("tmp", "kre"), ".jpg", 0600);
@@ -160,35 +91,51 @@ QString KreExporter::createKRE(const int recipeId)
       fn->close();
       xml += KCodecs::base64Encode(data, true);
     }
+    delete fn;
+
     xml += "]]></pic>\n";
     xml += "</pictures>\n";
     xml += "<category>\n";
-    for (Element *ct = (recipe->categoryList).getFirst(); ct; ct= (recipe->categoryList).getNext()){
-      xml += "<cat>"+ct->name.utf8()+"</cat>\n";
-    }
+
+	QPtrListIterator<Element> cat_it( recipe->categoryList );
+	Element *cat;
+	while ( (cat = cat_it.current()) != 0 )
+	{
+		++cat_it;
+		xml += "<cat>"+cat->name.utf8()+"</cat>\n";
+	}
+
     xml += "</category>\n";
     xml += "<serving>";
     xml += QString::number(recipe->persons);
     xml += "</serving>\n";
     xml += "</krecipes-description>\n";
     xml += "<krecipes-ingredients>\n";
-    for (Ingredient* in = (recipe->ingList).getFirst(); in; in= (recipe->ingList).getNext()){
+
+	QPtrListIterator<Ingredient> ing_it( recipe->ingList );
+	Ingredient *ing;
+	while ( (ing = ing_it.current()) != 0 )
+	{
+		++ing_it;
       xml += "<ingredient>\n";
-      xml += "<name>"+in->name.utf8()+"</name>\n";
+      xml += "<name>"+ing->name.utf8()+"</name>\n";
       xml += "<amount>";
-      xml += QString::number(in->amount);
+      xml += QString::number(ing->amount);
       xml += "</amount>\n";
-      xml += "<unit>"+in->units.utf8()+"</unit>\n";
+      xml += "<unit>"+ing->units.utf8()+"</unit>\n";
       xml += "</ingredient>\n";
       /// @todo add ingredient properties
-    }
+	}
+
     xml += "</krecipes-ingredients>\n";
     xml += "<krecipes-instructions>\n";
     xml += recipe->instructions.utf8();
     xml += "</krecipes-instructions>\n";
     xml += "</krecipes-recipe>\n";
-    if(recipeId != -1){
-      xml += "</krecipes>\n";
     }
+    
+    xml += "</krecipes>\n";
+
     return xml;
 }
+
--- src/exporters/kreexport.h
+++ src/exporters/kreexport.h
@@ -7,49 +7,28 @@
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  ***************************************************************************/
+
 #ifndef KREEXPORTER_H
 #define KREEXPORTER_H
 
-#include <kglobal.h>
-#include <klocale.h>
-#include <ktempfile.h>
-#include <kstandarddirs.h>
-#include <kmdcodec.h>
-#include <ktar.h>
-#include <kmessagebox.h>
+#include "baseexporter.h"
 
-#include <qfile.h>
-#include <qfileinfo.h>
-#include <qimage.h>
-#include <qvaluelist.h>
-
-#include "../recipe.h"
-#include "../DBBackend/recipedb.h"
-
-class Recipe;
 class RecipeDB;
 /**
 Export classe for Krecipes native file format (.kre, .kreml)
 
 @author Cyril Bosselut
 */
-class KreExporter{
+class KreExporter : public BaseExporter{
 public:
     KreExporter( RecipeDB *, const QString&, const QString );
 
-    ~KreExporter();
-    void exporter( const int );
-    void categoryExporter(QValueList<int>*);
-    QString createKRE(const int recipeid = -1);
+    virtual ~KreExporter();
 
-    QFile* file;
-    QString name;
-    QString type;
+    QString createKRE( const QPtrList<Recipe> & );
 
-private:
-    Recipe *recipe;
-    RecipeDB *database;
-    bool exportEnable;
+protected:
+    virtual void saveToFile( const QPtrList<Recipe> & );
 };
 
 #endif
