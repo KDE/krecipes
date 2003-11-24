e6aed69595def283c9f9b3c15bf396fadad256dc
Jason Kivlighn <jkivlighn@gmail.com>
2003-11-24 04:25:57 +0000

    CVS_SILENT Add copyright notice
    svn path=/trunk/kdeextragear-3/krecipes/; revision=269213

--- src/mmdata.h
+++ src/mmdata.h
@@ -1,3 +1,17 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by                                                 *
+ *   Jason Kivlighn (mizunoami44@users.sourceforge.net)                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+/** This file contains each of the Meal-Master unit abbreviations,
+  * and their cooresponding expansions.
+  */
+
 struct expand_unit_info
 {
    const char *short_form;
@@ -44,4 +58,3 @@ static expand_unit_info unit_info[] = {
   {"", "", ""},
   { 0, 0, 0 }
 };
-
