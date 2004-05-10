c72e1a493a3bbc450518e0b9e0cc734e26eeb515
Jason Kivlighn <jkivlighn@gmail.com>
2004-05-10 02:50:59 +0000

    CVS_SILENT: Up the version to 0.5 :)
    svn path=/trunk/kdeextragear-3/krecipes/; revision=309889

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@
 static const char *description =
     I18N_NOOP("A KDE Application");
 
-static const char *version = "0.4";
+static const char *version = "0.5";
 
 static KCmdLineOptions options[] =
 {
