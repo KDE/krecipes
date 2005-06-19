56ad41c4f7c3ab1c758486a74daefa8452d7ff05
Jason Kivlighn <jkivlighn@gmail.com>
2005-06-19 22:04:45 +0000

    SVN_SILENT: Update my email address, and 0.7 -> 0.8
    svn path=/trunk/extragear/utils/krecipes/; revision=427190

--- src/main.cpp
+++ src/main.cpp
@@ -29,9 +29,9 @@ static KCmdLineOptions options[] =
 int main( int argc, char **argv )
 {
 	KAboutData about( "krecipes", I18N_NOOP( "Krecipes" ), version, description,
-	                  KAboutData::License_GPL, I18N_NOOP( "(C) 2003 Unai Garro\n(C) 2004-2005 Jason Kivlighn\n\n___________\n\n\nThis product is RecipeML compatible.\n You can get more information about this file format in:\n http://www.formatdata.com/recipeml" ), 0, 0, "mizunoami44@users.sourceforge.net" );
+	                  KAboutData::License_GPL, I18N_NOOP( "(C) 2003 Unai Garro\n(C) 2004-2005 Jason Kivlighn\n\n___________\n\n\nThis product is RecipeML compatible.\n You can get more information about this file format in:\n http://www.formatdata.com/recipeml" ), 0, 0, "jkivlighn@gmail.com" );
 	about.addAuthor( "Unai Garro", 0, "ugarro@users.sourceforge.net" );
-	about.addAuthor( "Jason Kivlighn", 0, "mizunoami44@users.sourceforge.net" );
+	about.addAuthor( "Jason Kivlighn", 0, "jkivlighn@gmail.com" );
 	about.addAuthor( "Cyril Bosselut", 0, "bosselut@b1project.com" );
 	about.setTranslator( I18N_NOOP( "INSERT YOUR NAME HERE" ), I18N_NOOP( "INSERT YOUR EMAIL ADDRESS" ) );
 	KCmdLineArgs::init( argc, argv, &about );
