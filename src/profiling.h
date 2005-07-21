35fa2d143569558e03d8fb15b57abb8fa0d884c7
Jason Kivlighn <jkivlighn@gmail.com>
2005-07-21 02:41:22 +0000

    SVN_SILENT: Add copyright
    svn path=/trunk/extragear/utils/krecipes/; revision=437143

--- src/profiling.h
+++ src/profiling.h
@@ -1,3 +1,13 @@
+/***************************************************************************
+*   Copyright (C) 2005 by                                                 *
+*   Jason Kivlighn (jkivlighn@gmail.com)                                  *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+***************************************************************************/
+
 #ifndef PROFILING_H
 #define PROFILING_H
 
