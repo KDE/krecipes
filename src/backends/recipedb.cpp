bd0b77b8b18dc1356de0efc2359c32ef1a2f146e
José Manuel Santamaría Lema <panfaust@gmail.com>
2009-06-23 00:51:57 +0000

    Add myself as copyright holder.
    svn path=/trunk/extragear/utils/krecipes/; revision=985512

--- src/backends/recipedb.cpp
+++ src/backends/recipedb.cpp
@@ -4,6 +4,7 @@
 *   Cyril Bosselut (bosselut@b1project.com)                               *
 *                                                                         *
 *   Copyright (C) 2003-2006 Jason Kivlighn (jkivlighn@gmail.com)          *
+*   Copyright (C) 2009 José Manuel Santamaría Lema (panfaust@gmail.com)   *
 *                                                                         *
 *   This program is free software; you can redistribute it and/or modify  *
 *   it under the terms of the GNU General Public License as published by  *
