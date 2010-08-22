97f1b29ce02346385aebada269cfcd97497a2d83
Albert Astals Cid <tsdgeos@terra.es>
2010-08-22 13:06:34 +0000

    fix extractrc calls
    svn path=/trunk/extragear/utils/krecipes/; revision=1166676

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find -name \*.ui` >> rc.cpp || exit 11
+$EXTRACTRC `find -name \*.ui -o -name \*.rc` >> rc.cpp || exit 11
 $XGETTEXT `find . -name "*.cpp" -o -name "*.h" ` -o $podir/krecipes.pot
 rm -f rc.cpp
