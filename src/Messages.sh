79f83e83571ca6e825f2b570a168dcf0d703d617
Pino Toscano <pino@kde.org>
2010-11-01 11:21:21 +0000

    do not extract messages from sources in 'tests' subdirectories
    svn path=/trunk/extragear/utils/krecipes/; revision=1191888

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find -name \*.ui -o -name \*.rc` >> rc.cpp || exit 11
-$XGETTEXT `find . -name "*.cpp" -o -name "*.h" ` -o $podir/krecipes.pot
+$XGETTEXT `find . -name "*.cpp" -o -name "*.h" | grep -v '/tests/'` -o $podir/krecipes.pot
 rm -f rc.cpp
