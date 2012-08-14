0f630c7b1045a5defa11bbfbe1ec7758351808fa
José Manuel Santamaría Lema <panfaust@gmail.com>
2012-08-14 17:19:50 +0200

    Do not include NukeDB files in translations.
    It's a tool meant to be used only by the Krecipes developer(s).

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find -name \*.ui -o -name \*.rc` >> rc.cpp || exit 11
-$XGETTEXT `find . -name "*.cpp" -o -name "*.h" | grep -v '/tests/'` -o $podir/krecipes.pot
+$EXTRACTRC `find -name \*.ui -o -name \*.rc | grep -v '/nukedb/'` >> rc.cpp || exit 11
+$XGETTEXT `find . -name "*.cpp" -o -name "*.h" \
+| grep -v '/tests/' | grep -v '/nukedb/'` -o $podir/krecipes.pot
 rm -f rc.cpp
