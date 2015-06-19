cda29d69c85663db4a626039f23574cccedcf270
Albert Astals Cid <aacid@kde.org>
2015-06-19 22:34:56 +0200

    Make sure string is of enough length before asking for character 11
    BUGS: 260997
    FIXED-IN: 2.2

--- src/importers/mmfimporter.cpp
+++ src/importers/mmfimporter.cpp
@@ -169,7 +169,7 @@ bool MMFImporter::loadIngredientLine( const QString &string, IngredientList &lis
 	Ingredient new_ingredient;
 	new_ingredient.amount = 0; //amount not required, so give default of 0
 
-	if ( string.at( 11 ) == '-' && string.mid( 0, 11 ).trimmed().isEmpty() && !list.isEmpty() )  //continuation of previous ingredient
+	if ( string.length() > 11 && string.at( 11 ) == '-' && string.mid( 0, 11 ).trimmed().isEmpty() && !list.isEmpty() )  //continuation of previous ingredient
 	{
 		//kDebug()<<"Appending to last ingredient in column: "<<string.trimmed().mid(1,string.length());
 		list.last().name += ' ' + string.trimmed().mid( 1, string.length() );
