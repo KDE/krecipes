a481d6218f39c34ba8e7ec6eabf859e5dc364211
José Manuel Santamaría Lema <panfaust@gmail.com>
2016-02-18 10:42:09 +0100

    Remove Qt3 suport from the shopping list dialog.
    TODO: Drag & Drop

--- src/dialogs/shoppinglistdialog.cpp
+++ src/dialogs/shoppinglistdialog.cpp
@@ -2,6 +2,7 @@
 *   Copyright © 2003 Unai Garro <ugarro@gmail.com>                        *
 *   Copyright © 2003 Cyril Bosselut <bosselut@b1project.com>              *
 *   Copyright © 2003 Jason Kivlighn <jkivlighn@gmail.com>                 *
+*   Copyright © 2016 José Manuel Santamaría Lema <panfaust@gmail.com>     *
 *                                                                         *
 *   This program is free software; you can redistribute it and/or modify  *
 *   it under the terms of the GNU General Public License as published by  *
@@ -11,52 +12,21 @@
 
 #include "shoppinglistdialog.h"
 
-#include <klocale.h>
-#include <kconfig.h>
-#include <kcursor.h>
-#include <kdialog.h>
-#include <kglobal.h>
-#include <kapplication.h>
-#include <KConfigGroup>
-
-#include "backends/recipedb.h"
 #include "refineshoppinglistdialog.h"
-#include "datablocks/recipelist.h"
+#include "widgets/krerecipeslistwidget.h"
+#include "widgets/kresearchresultlistwidget.h"
 #include "widgets/recipelistview.h"
-#include "recipefilter.h"
 #include "actionshandlers/recipeactionshandler.h"
+#include "backends/recipedb.h"
+#include "datablocks/recipelist.h"
+
+#include <KLocale>
+#include <KPushButton>
+#include <KVBox>
+
 #include <QDropEvent>
 #include <QGridLayout>
 #include <QVBoxLayout>
-#include <kvbox.h>
-
-/** A simple listview to accept dropping a RecipeItemDrag */
-class ShoppingListView : public K3ListView
-{
-public:
-	ShoppingListView( QWidget *parent ) : K3ListView( parent )
-	{}
-
-protected:
-	bool acceptDrag( QDropEvent *event ) const
-	{
-		return RecipeItemDrag::canDecode( event );
-	}
-
-	Q3DragObject *dragObject()
-	{
-		RecipeListItem * item = dynamic_cast<RecipeListItem*>( selectedItem() );
-		if ( item != 0 ) {
-
-			RecipeItemDrag * obj = new RecipeItemDrag( item, this, "Recipe drag item" );
-			/*const QPixmap *pm = item->pixmap(0);
-			if( pm )
-				obj->setPixmap( *pm );*/
-			return obj;
-		}
-		return 0;
-	}
-};
 
 
 ShoppingListDialog::ShoppingListDialog( QWidget *parent, RecipeDB *db ) : QWidget( parent )
@@ -68,60 +38,26 @@ ShoppingListDialog::ShoppingListDialog( QWidget *parent, RecipeDB *db ) : QWidge
 	layout = new QGridLayout( this );
 	layout->cellRect( 2, 2 );
 
-	recipeListView = new KreListView ( this, i18nc( "@title", "Full recipe list" ), true, 1 );
-	layout->addWidget( recipeListView, 0, 0 );
-	listview = new RecipeListView( recipeListView, database );
-	listview->setSizePolicy( QSizePolicy::Ignored, QSizePolicy::MinimumExpanding );
-	listview->setDragEnabled( true );
-	listview->setAcceptDrops( true );
-	listview->setDropVisualizer( false );
-
-	//FIXME: Connecting to ensurePopulated here is so disgusting, RecipeFilter should handle it.
-	KConfigGroup config (KGlobal::config(), "Performance" );
-	if ( config.readEntry("SearchAsYouType",true) )
-		connect( recipeListView, SIGNAL( textChanged(const QString&) ), SLOT( ensurePopulated() ) );
-	else
-		connect( recipeListView, SIGNAL( returnPressed(const QString&) ), SLOT( ensurePopulated() ) );
-
-	connect( listview, SIGNAL( dropped( K3ListView*, QDropEvent*, Q3ListViewItem* ) ),
-		      this, SLOT( slotDropped( K3ListView*, QDropEvent*, Q3ListViewItem* ) ) );
-	recipeListView->setListView( listview );
-	recipeListView->setCustomFilter( new RecipeFilter( recipeListView->listView() ), SLOT( filter( const QString & ) ) );
+	m_sourceListWidget = new KreRecipesListWidget( this, database );
+	m_sourceListWidget->setListTitle( i18nc( "@title", "Full recipe list" ) );
+	layout->addWidget( m_sourceListWidget, 0, 0 );
 
 	QVBoxLayout* vboxl = new QVBoxLayout();
 	vboxl->addStretch();
 	addRecipeButton = new KPushButton( this );
 	addRecipeButton->setIcon( KIcon( "arrow-right" ) );
-	addRecipeButton->setFixedSize( QSize( 32, 32 ) );
 	vboxl->addWidget( addRecipeButton );
 
 	removeRecipeButton = new KPushButton( this );
 	removeRecipeButton->setIcon( KIcon( "arrow-left" ) );
-	removeRecipeButton->setFixedSize( QSize( 32, 32 ) );
 	vboxl->addWidget( removeRecipeButton );
 	vboxl->addStretch();
 
 	layout->addLayout( vboxl, 0, 1 );
 
-	shopRecipeListView = new KreListView ( this, i18nc("@title", "Shopping List") );
-	ShoppingListView *slistview = new ShoppingListView( shopRecipeListView );
-	slistview->setDragEnabled( true );
-	slistview->setAcceptDrops( true );
-	slistview->setDropVisualizer( false );
-	connect( slistview, SIGNAL( dropped( K3ListView*, QDropEvent*, Q3ListViewItem* ) ),
-		      this, SLOT( slotDropped( K3ListView*, QDropEvent*, Q3ListViewItem* ) ) );
-	shopRecipeListView->setListView( slistview );
-	layout->addWidget( shopRecipeListView, 0, 2 );
-
-	shopRecipeListView->listView() ->addColumn( i18nc( "@title:column", "Recipe Title" ) );
-
-	config.changeGroup( "Advanced" );
-	bool show_id = config.readEntry( "ShowID", false );
-	shopRecipeListView->listView() ->addColumn( i18nc( "@title:column", "Id" ) , show_id ? -1 : 0 );
-
-	shopRecipeListView->listView() ->setSorting( -1 );
-	shopRecipeListView->setSizePolicy( QSizePolicy::Minimum, QSizePolicy::MinimumExpanding );
-	shopRecipeListView->listView() ->setAllColumnsShowFocus( true );
+	m_destListwidget = new KreSearchResultListWidget( this, database );
+	m_destListwidget->setListTitle( i18nc("@title", "Shopping List") );
+	layout->addWidget( m_destListwidget, 0, 2 );
 
 	buttonBar = new KHBox( this );
 	layout->addWidget( buttonBar, 1, 1, 1, 2, 0 );
@@ -142,9 +78,6 @@ ShoppingListDialog::ShoppingListDialog( QWidget *parent, RecipeDB *db ) : QWidge
 	clearButton->setText( i18nc( "@action:button Clear list", "Clear" ) );
 	clearButton->setIcon( KIcon( "edit-clear" ) );
 
-	//Takes care of all recipe actions and provides a popup menu to 'recipeListView'
-	actionHandler = new RecipeActionsHandler( recipeListView->listView(), database );
-
 	// Connect signals & slots
 	connect( addRecipeButton, SIGNAL( clicked() ), this, SLOT( addRecipe() ) );
 	connect( removeRecipeButton, SIGNAL( clicked() ), this, SLOT( removeRecipe() ) );
@@ -155,11 +88,6 @@ ShoppingListDialog::ShoppingListDialog( QWidget *parent, RecipeDB *db ) : QWidge
 ShoppingListDialog::~ShoppingListDialog()
 {}
 
-void ShoppingListDialog::ensurePopulated()
-{
-	listview->populateAll();
-}
-
 void ShoppingListDialog::createShopping( const RecipeList &rlist )
 {
 	clear();
@@ -169,60 +97,42 @@ void ShoppingListDialog::createShopping( const RecipeList &rlist )
 	}
 }
 
-void ShoppingListDialog::reloadRecipeList( ReloadFlags flag )
-{
-	( ( RecipeListView* ) recipeListView->listView() ) ->reload( flag );
-}
-
 void ShoppingListDialog::reload( ReloadFlags flag )
 {
-	reloadRecipeList ( flag ); // Missing: check if there's non-existing recipes in the list now, and if so, delete.
+	m_sourceListWidget->reload( flag );
 }
 
 void ShoppingListDialog::addRecipe( void )
 {
-	QList<Q3ListViewItem*> items = recipeListView->listView()->selectedItems();
+	QList<RecipeDB::IdType> id_list;
+	QList<RecipeDB::IdType>::iterator id_list_it;
+	RecipeList recipe_list;
 
-	     for ( int i = 0;i <items.count();i++ )
-	     {
-	         addRecipe( items.at( i ) );
-
-	     }
-}
-
-void ShoppingListDialog::addRecipe( Q3ListViewItem *item )
-{
-	if ( item ) {
-		if ( item->rtti() == 1000 ) {
-			RecipeListItem * recipe_it = ( RecipeListItem* ) item;
-
-			Recipe r;
-			r.title = recipe_it->title();
-			r.recipeID = recipe_it->recipeID();
-			( void ) new RecipeListItem( shopRecipeListView->listView(), r );
-		}
+	id_list = m_sourceListWidget->selectedRecipes();
+	for( id_list_it = id_list.begin(); id_list_it != id_list.end(); id_list_it++) {
+		Recipe recipe;
+		recipe.recipeID = *id_list_it;
+		recipe.title = m_sourceListWidget->getRecipeName(*id_list_it);
+		recipe_list << recipe;
 	}
+
+	m_destListwidget->displayRecipes( recipe_list );
 }
 
 void ShoppingListDialog::removeRecipe( void )
 {
-	Q3ListViewItem * it;
-	it = shopRecipeListView->listView() ->selectedItem();
-	if ( it )
-		delete it;
+	QList<int> selected_rows = m_destListwidget->currentRows();
+
+	QList<int>::iterator it;
+	for ( it = selected_rows.begin(); it != selected_rows.end(); it ++)
+	{
+		m_destListwidget->model()->removeRow( *it );
+	}
 }
 
 void ShoppingListDialog::showShoppingList( void )
 {
-	// Store the recipe list in ElementList object first
-	ElementList recipeList;
-	RecipeListItem *it;
-	for ( it = ( RecipeListItem* ) shopRecipeListView->listView() ->firstChild();it;it = ( RecipeListItem* ) it->nextSibling() ) {
-		Element newEl;
-		newEl.id = it->recipeID();
-		newEl.name = it->title(); // Storing the title is not necessary, but do it just in case it's used later on
-		recipeList.append( newEl );
-	}
+	ElementList recipeList = m_destListwidget->displayedRecipes();
 
 	QPointer<RefineShoppingListDialog> refineDialog = new RefineShoppingListDialog( this, database, recipeList );
 	refineDialog->exec();
@@ -235,40 +145,15 @@ void ShoppingListDialog::addRecipeToShoppingList( int recipeID )
 	r.title = database->recipeTitle( recipeID );
 	r.recipeID = recipeID;
 
-	new RecipeListItem( shopRecipeListView->listView(), r );
+	RecipeList list;
+	list << r;
+
+	m_destListwidget->displayRecipes( list );
 }
 
 void ShoppingListDialog::clear()
 {
-	shopRecipeListView->listView() ->clear();
-}
-
-void ShoppingListDialog::slotDropped( K3ListView *list, QDropEvent *e, Q3ListViewItem * /*after*/ )
-{
-	Recipe r;
-	RecipeListItem *item = new RecipeListItem( recipeListView->listView(), r ); // needs parent, use this temporarily
-	if ( !RecipeItemDrag::decode( e, *item ) ) {
-		delete item;
-		return ;
-	}
-
-	if ( list == shopRecipeListView->listView() ) {
-		addRecipe( item );
-	}
-	//find and delete the item if we just dropped onto the recipe list from the shopping list
-	else if ( list == recipeListView->listView() && e->source() == shopRecipeListView->listView() ) {
-		Q3ListViewItemIterator list_it = Q3ListViewItemIterator( shopRecipeListView->listView() );
-		while ( list_it.current() ) {
-			if ( ( ( RecipeListItem* ) list_it.current() ) ->recipeID() == item->recipeID() ) {
-				delete list_it.current();
-				break;
-			}
-			list_it++;
-		}
-	}
-
-	delete item;
-	item = 0; // not needed anymore
+	m_destListwidget->clear();
 }
 
 #include "shoppinglistdialog.moc"
--- src/dialogs/shoppinglistdialog.h
+++ src/dialogs/shoppinglistdialog.h
@@ -2,6 +2,7 @@
 *   Copyright © 2003 Unai Garro <ugarro@gmail.com>                        *
 *   Copyright © 2003 Cyril Bosselut <bosselut@b1project.com>              *
 *   Copyright © 2003 Jason Kivlighn <jkivlighn@gmail.com>                 *
+*   Copyright © 2016 José Manuel Santamaría Lema <panfaust@gmail.com>     *
 *                                                                         *
 *   This program is free software; you can redistribute it and/or modify  *
 *   it under the terms of the GNU General Public License as published by  *
@@ -12,14 +13,6 @@
 #ifndef SHOPPINGLISTDIALOG_H
 #define SHOPPINGLISTDIALOG_H
 
-
-//Added by qt3to4:
-#include <QGridLayout>
-#include <QDropEvent>
-#include <kpushbutton.h>
-#include <kiconloader.h>
-#include <kvbox.h>
-
 #include "datablocks/recipelist.h"
 #include "widgets/krelistview.h"
 #include "widgets/dblistviewbase.h"
@@ -28,6 +21,12 @@ class RecipeDB;
 class ShoppingListViewDialog;
 class RecipeActionsHandler;
 class RecipeListView;
+
+class KreRecipesListWidget;
+class KreSearchResultListWidget;
+
+class QGridLayout;
+class KPushButton;
 
 /**
 @author Unai Garro
@@ -47,9 +46,6 @@ private:
 
 	// Internal variables
 	RecipeDB *database;
-	// Internal Methods
-	void reloadRecipeList( ReloadFlags flag = Load );
-	void addRecipe( Q3ListViewItem *item );
 	// Widgets
 	QGridLayout* layout;
 	KPushButton* addRecipeButton;
@@ -59,9 +55,8 @@ private:
 	KHBox *buttonBar;
 	KPushButton* okButton;
 	KPushButton* clearButton;
-	ShoppingListViewDialog *shoppingListDisplay;
-	RecipeActionsHandler *actionHandler;
-	RecipeListView *listview;
+	KreRecipesListWidget * m_sourceListWidget;
+	KreSearchResultListWidget * m_destListwidget;
 
 public slots:
 	void addRecipeToShoppingList( int recipeID ); // Called from inputdialog-> through krecipesview
@@ -71,8 +66,6 @@ private slots:
 	void removeRecipe( void );
 	void showShoppingList( void );
 	void clear( void );
-	void slotDropped( K3ListView *list, QDropEvent *e, Q3ListViewItem *after );
-	void ensurePopulated();
 };
 
 #endif
