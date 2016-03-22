c6c4f9da8cd640090a9ba3a3f11c02afaf4949db
José Manuel Santamaría Lema <panfaust@gmail.com>
2016-03-22 19:27:09 +0100

    Add NutrientInfoStatus stub.

---
+++ src/dialogs/recipeinput/nutrientinfostatuswidget.h
@@ -0,0 +1,31 @@
+/****************************************************************************
+*   Copyright © 2016 José Manuel Santamaría Lema <panfaust@gmail.com>       *
+*                                                                           *
+*   This program is free software; you can redistribute it and/or modify    *
+*   it under the terms of the GNU General Public License as published by    *
+*   the Free Software Foundation; either version 2 of the License, or       *
+*   (at your option) any later version.                                     *
+****************************************************************************/
+
+#ifndef NUTRIENTINFOSTATUSWIDGET_H
+#define NUTRIENTINFOSTATUSWIDGET_H
+
+#include <QWidget>
+
+namespace Ui {
+	class NutrientInfoStatusWidget;
+}
+
+class NutrientInfoStatusWidget : public QWidget
+{
+	Q_OBJECT
+
+public:
+	NutrientInfoStatusWidget( QWidget * parent = 0 );
+
+private:
+	Ui::NutrientInfoStatusWidget * ui;
+
+};
+
+#endif
